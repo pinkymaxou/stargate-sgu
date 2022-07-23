77fc39c369ddbf2204a62c10079114673e28146d
Max <pinkymaxou08@gmail.com>
2022-07-22 22:52:47 -0400

    New gray wormhole

--- Source/base-fw/main/Wormhole.c
+++ Source/base-fw/main/Wormhole.c
@@ -17,11 +17,16 @@ static void DoRing1();
 static void DoRing2();
 static void DoRing3();
 
+static int GetRing(int zeroBasedIndex);
+
 // 0 = Exterior <====> 3 = interior
+// One based, but I should have made it 0 based like a respectable programmer.
 static const int m_pRing0[] = { 1, 2, 3, 4, 5, 6, 7, 8, 13, 14, 15, 16, 17, 18, 19, 47, 48 };
 static const int m_pRing1[] = { 9, 10, 11, 12, 20, 21, 22, 23, 24, 25, 26, 27, 31, 32, 33, 34, 35, 36 };
 static const int m_pRing2[] = { 37, 38, 39, 40, 28, 29, 30, 42, 43 };
 static const int m_pRing3[] = { 41, 44, 45, 46  };
+
+#define MIN(X,Y) (((X) < (Y)) ? (X) : (Y))
 
 static SLedEffect m_sLedEffects[FWCONFIG_WORMHOLELEDS_LEDCOUNT];
 
@@ -48,7 +53,7 @@ void WORMHOLE_Run(volatile bool* pIsCancelled, bool bNoTimeLimit)
     const uint32_t u32MaxBrightness = SETTINGS_GetValueInt32(SETTINGS_EENTRY_WormholeMaxBrightness);
 
     const float minF = 0.10f;
-    const float maxF = 0.20f;
+    const float maxF = 0.25f;
 
     for(int i = 0; i < FWCONFIG_WORMHOLELEDS_LEDCOUNT; i++)
     {
@@ -67,7 +72,25 @@ void WORMHOLE_Run(volatile bool* pIsCancelled, bool bNoTimeLimit)
         {
             SLedEffect* psLedEffect = &m_sLedEffects[i];
 
-            GPIO_SetPixel(i, psLedEffect->fOne*u32MaxBrightness, psLedEffect->fOne*u32MaxBrightness, psLedEffect->fOne*u32MaxBrightness);
+            const int d = GetRing(i);
+            float fFactor = 1.0f;
+            switch(d)
+            {
+                case 3:
+                    fFactor = 2.5f;
+                    break;
+                case 2:
+                    fFactor = 1.5f;
+                    break;
+                case 1:
+                    fFactor = 0.6f;
+                    break;
+                case 0:
+                    fFactor = 0.25f;
+                    break;
+            }
+
+            GPIO_SetPixel(i, MIN(psLedEffect->fOne*u32MaxBrightness * fFactor, 255), MIN(psLedEffect->fOne*u32MaxBrightness * fFactor, 255), MIN(psLedEffect->fOne*u32MaxBrightness * fFactor, 255));
 
             const float fInc = 0.005f + ( 0.01f * (rand() % 100) ) * 0.001f;
 
@@ -91,6 +114,27 @@ void WORMHOLE_Run(volatile bool* pIsCancelled, bool bNoTimeLimit)
         GPIO_RefreshPixels();
         vTaskDelay(pdMS_TO_TICKS(20));
     }
+}
+
+static int GetRing(int zeroBasedIndex)
+{
+    for(int j = 0; j < sizeof(m_pRing0)/sizeof(int); j++)
+        if (m_pRing0[j]-1 == zeroBasedIndex)
+            return 0;
+
+    for(int j = 0; j < sizeof(m_pRing1)/sizeof(int); j++)
+        if (m_pRing1[j]-1 == zeroBasedIndex)
+            return 1;
+
+    for(int j = 0; j < sizeof(m_pRing2)/sizeof(int); j++)
+        if (m_pRing2[j]-1 == zeroBasedIndex)
+            return 2;
+
+    for(int j = 0; j < sizeof(m_pRing3)/sizeof(int); j++)
+        if (m_pRing3[j]-1 == zeroBasedIndex)
+            return 3;
+
+    return 0;
 }
 
 void WORMHOLE_Close(volatile bool* pIsCancelled)
