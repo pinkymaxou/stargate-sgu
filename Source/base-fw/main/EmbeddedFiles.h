60df04a231baf7b3c3e5a7b5a55e11c44d5fb2c4
Max <pinkymaxou08@gmail.com>
2022-07-24 11:55:50 -0400

    First draft of OTA separation

--- Source/base-fw/main/EmbeddedFiles.h
+++ Source/base-fw/main/EmbeddedFiles.h
@@ -19,20 +19,24 @@ typedef struct
 
 typedef enum
 {
-    EF_EFILE_CONFIG_JSON = 0,    /*!< @brief File: config.json (size: 18 KB) */
-    EF_EFILE_FAVICON_ICO = 1,    /*!< @brief File: favicon.ico (size: 1 KB) */
-    EF_EFILE_INDEX_HTML = 2,    /*!< @brief File: index.html (size: 4 KB) */
-    EF_EFILE_CSS_CONTENT_CSS = 3,    /*!< @brief File: css/content.css (size: 2 KB) */
-    EF_EFILE_FONT_ANCIENT_VIRTUAL_TTF_WOFF = 4,    /*!< @brief File: font/ancient_virtual.ttf.woff (size: 11 KB) */
-    EF_EFILE_FONT_EUROSTILE_EXTENDED2_BOLD_WOFF = 5,    /*!< @brief File: font/eurostile_extended2_bold.woff (size: 14 KB) */
-    EF_EFILE_FONT_STARGATEUNIVERSE_WOFF = 6,    /*!< @brief File: font/stargateuniverse.woff (size: 6 KB) */
-    EF_EFILE_FONT_COPYRIGHTS_ANCIENT_VIRTUAL_COPYRIGHT_TXT = 7,    /*!< @brief File: font/copyrights/ancient_virtual_copyright.txt (size: 46  B) */
-    EF_EFILE_FONT_COPYRIGHTS_EUROSTILE_EXTENDED2_BOLD_COPYRIGHT_HTML = 8,    /*!< @brief File: font/copyrights/eurostile_extended2_bold_copyright.html (size: 5 KB) */
-    EF_EFILE_FONT_COPYRIGHTS_STARGATEUNIVERSE_LISEZ_MOI_TXT = 9,    /*!< @brief File: font/copyrights/stargateuniverse-lisez-moi.txt (size: 2 KB) */
-    EF_EFILE_FONT_COPYRIGHTS_STARGATEUNIVERSE_README_TXT = 10,    /*!< @brief File: font/copyrights/stargateuniverse-readme.txt (size: 2 KB) */
-    EF_EFILE_JS_APP_JS = 11,    /*!< @brief File: js/app.js (size: 5 KB) */
-    EF_EFILE_JS_VUE_MIN_JS = 12,    /*!< @brief File: js/vue.min.js (size: 92 KB) */
-    EF_EFILE_COUNT = 13
+    EF_EFILE_ABOUT_OTA_HTML = 0,    /*!< @brief File: about-ota.html (size: 1 KB) */
+    EF_EFILE_CONFIG_JSON = 1,    /*!< @brief File: config.json (size: 18 KB) */
+    EF_EFILE_FAVICON_ICO = 2,    /*!< @brief File: favicon.ico (size: 1 KB) */
+    EF_EFILE_INDEX_HTML = 3,    /*!< @brief File: index.html (size: 3 KB) */
+    EF_EFILE_SETTINGS_HTML = 4,    /*!< @brief File: settings.html (size: 1 KB) */
+    EF_EFILE_CSS_CONTENT_CSS = 5,    /*!< @brief File: css/content.css (size: 2 KB) */
+    EF_EFILE_FONT_ANCIENT_VIRTUAL_TTF_WOFF = 6,    /*!< @brief File: font/ancient_virtual.ttf.woff (size: 11 KB) */
+    EF_EFILE_FONT_EUROSTILE_EXTENDED2_BOLD_WOFF = 7,    /*!< @brief File: font/eurostile_extended2_bold.woff (size: 14 KB) */
+    EF_EFILE_FONT_STARGATEUNIVERSE_WOFF = 8,    /*!< @brief File: font/stargateuniverse.woff (size: 6 KB) */
+    EF_EFILE_FONT_COPYRIGHTS_ANCIENT_VIRTUAL_COPYRIGHT_TXT = 9,    /*!< @brief File: font/copyrights/ancient_virtual_copyright.txt (size: 46  B) */
+    EF_EFILE_FONT_COPYRIGHTS_EUROSTILE_EXTENDED2_BOLD_COPYRIGHT_HTML = 10,    /*!< @brief File: font/copyrights/eurostile_extended2_bold_copyright.html (size: 5 KB) */
+    EF_EFILE_FONT_COPYRIGHTS_STARGATEUNIVERSE_LISEZ_MOI_TXT = 11,    /*!< @brief File: font/copyrights/stargateuniverse-lisez-moi.txt (size: 2 KB) */
+    EF_EFILE_FONT_COPYRIGHTS_STARGATEUNIVERSE_README_TXT = 12,    /*!< @brief File: font/copyrights/stargateuniverse-readme.txt (size: 2 KB) */
+    EF_EFILE_JS_ABOUT_OTA_JS = 13,    /*!< @brief File: js/about-ota.js (size: 1 KB) */
+    EF_EFILE_JS_APP_JS = 14,    /*!< @brief File: js/app.js (size: 4 KB) */
+    EF_EFILE_JS_SETTINGS_JS = 15,    /*!< @brief File: js/settings.js (size: 314  B) */
+    EF_EFILE_JS_VUE_MIN_JS = 16,    /*!< @brief File: js/vue.min.js (size: 92 KB) */
+    EF_EFILE_COUNT = 17
 } EF_EFILE;
 
 /*! @brief Check if compressed flag is active */
--- Source/base-fw/main/www/js/app.js
+++ Source/base-fw/main/www/js/app.js
@@ -77,7 +77,6 @@ var currentApp = new Vue({
   {
     symbols: allsymbols,
     alladdresses: alladdresses,
-    sysinfos: [],
   }
 })
 
@@ -86,56 +85,3 @@ function GetSymbolIndex(SymbolOneBased)
   return allsymbols[SymbolOneBased-1];
 }
 
-function idBtnUpload_Click()
-{
-	console.log("idBtnUpload_Click");
-	
-	var xhr = new XMLHttpRequest();
-	xhr.open('POST', '/ota/upload', true);
-
-	let firmwareFile = document.getElementById("idFile").files[0];
-
-	// Listen to the upload progress.
-	xhr.upload.onprogress = function(e) 
-	{
-		console.log("upload in onprogress", e);
-	};
-	
-	xhr.upload.loadstart = function(e)
-	{
-		console.log("upload started", e);	
-	};
-
-	xhr.upload.loadend = function(e)
-	{
-		console.log("upload ended", e);	
-	};
-
-	xhr.upload.load  = function(e)
-	{
-		console.log("upload succeeded!", e);	
-	};
-
-	xhr.upload.error = function(e)
-	{
-		console.log("upload error", e);	
-	};
-
-	xhr.upload.abort = function(e)
-	{
-		console.log("upload aborted", e);	
-	};
-	
-	xhr.send(firmwareFile);
-}
-
-function AppLoaded()
-{
-  fetch('./api/getsysinfo')
-    .then((response) => response.json())
-    .then((data) =>
-    {
-      console.log(data);
-      currentApp.sysinfos = data.infos;
-    });
-}
