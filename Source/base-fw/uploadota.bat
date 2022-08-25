681620fdcdd1dbda76b6e90311dadaa5d220c4cb
Maxime Carrier <maxime.carrier@umanomed.onmicrosoft.com>
2022-08-25 12:24:00 -0400

    Update embedded resources

--- Source/base-fw/uploadota.bat
+++ Source/base-fw/uploadota.bat
@@ -1,2 +1,2 @@
 idf.py build
-..\tools\otauploader.py --input "build\base-fw.bin" --destaddr "http://172.16.42.17/ota/upload"
+python ..\tools\otauploader.py --input "build\base-fw.bin" --destaddr "http://172.16.42.17/ota/upload"
