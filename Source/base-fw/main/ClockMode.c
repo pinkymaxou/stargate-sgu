fcb55ce958669114a0cb0ce783e64572928acdf8
Maxime Carrier <maxime.carrier@umanomed.onmicrosoft.com>
2022-08-26 12:31:17 -0400

    Binary mode

--- Source/base-fw/main/ClockMode.c
+++ Source/base-fw/main/ClockMode.c
@@ -1,3 +1,5 @@
+#include <time.h>
+#include <math.h>
 #include "ClockMode.h"
 #include "GPIO.h"
 #include "freertos/FreeRTOS.h"
@@ -6,32 +8,64 @@
 #include "FWConfig.h"
 #include "Settings.h"
 
-static const int m_pRing0[] = { 1, 2, 3, 4, 5, 6, 7, 8, 13, 14, 15, 16, 17, 18, 19, 47, 48 };
-#define RING0_COUNT (sizeof(m_pRing0)/sizeof(m_pRing0[0]))
+static const int m_pSeconds[] = { 1, 2, 3, 4, 5, 6, 7, 8, 13, 14, 15, 16, 17, 18, 19, 47, 48 };
+#define SECONDS_COUNT (sizeof(m_pSeconds)/sizeof(m_pSeconds[0]))
+
+static const int m_pHours[] = { 35, 36, 20, 21, 22, 23 };
+#define HOURS_COUNT (sizeof(m_pHours)/sizeof(m_pHours[0]))
+
+static const int m_pMinutes[] = { 31, 12, 11, 10, 9, 27 };
+#define MINUTES_COUNT (sizeof(m_pMinutes)/sizeof(m_pMinutes[0]))
+
 
 void CLOCKMODE_Run(volatile bool* pIsCancelled)
 {
     GPIO_ClearAllPixels();
     GPIO_RefreshPixels();
 
-    int ix = 0;
-
     while(!*pIsCancelled)
     {
-        for(int i = 0; i < RING0_COUNT; i++)
-        {
-            int trueIndex = m_pRing0[i] - 1;
+        time_t now = 0;
+        struct tm timeinfo = { 0 };
+        time(&now);
+        localtime_r(&now, &timeinfo);
 
-            if (i == ix)
-                GPIO_SetPixel(i, 255, 0, 0);
+        for(int i = 0; i < HOURS_COUNT; i++)
+        {
+            int trueIndex = m_pHours[i] - 1;
+
+            if (timeinfo.tm_hour & (2 << i))
+                GPIO_SetPixel(trueIndex, 0, 255, 0);
             else
-                GPIO_SetPixel(i, 0, 0, 255);
+                GPIO_SetPixel(trueIndex, 40, 40, 40);
         }
 
-        ix = (ix + 1) % RING0_COUNT;
+        // Minutes
+        for(int i = 0; i < MINUTES_COUNT; i++)
+        {
+            int trueIndex = m_pMinutes[i] - 1;
+
+            if (timeinfo.tm_min & (2 << i))
+                GPIO_SetPixel(trueIndex, 0, 255, 0);
+            else
+                GPIO_SetPixel(trueIndex, 40, 40, 40);
+        }
+
+        // Seconds
+        const float fltSeconds = ((float)timeinfo.tm_sec / 60.0f) * SECONDS_COUNT;
+
+        for(int i = 0; i < SECONDS_COUNT; i++)
+        {
+            int trueIndex = m_pSeconds[i] - 1;
+
+            if (i < (int)fltSeconds)
+                GPIO_SetPixel(trueIndex, 255, 0, 255);
+            else
+                GPIO_SetPixel(trueIndex, 10, 10, 10);
+        }
 
         GPIO_RefreshPixels();
-        vTaskDelay(pdMS_TO_TICKS(200));
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
 
     GPIO_ClearAllPixels();
