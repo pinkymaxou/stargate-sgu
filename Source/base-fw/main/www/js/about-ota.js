9c58a29dca7764873b2b5856bee221269fe759a2
Maxime Carrier <maxime.carrier@umanomed.onmicrosoft.com>
2022-10-14 21:43:06 -0400

    OTA upgrade

--- Source/base-fw/main/www/js/about-ota.js
+++ Source/base-fw/main/www/js/about-ota.js
@@ -1,44 +1,39 @@
 function idBtnUpload_Click()
 {
-	console.log("idBtnUpload_Click");
-	
-	var xhr = new XMLHttpRequest();
-	xhr.open('POST', '/ota/upload', true);
+	try {
+		let lblStatus = document.getElementById("lblStatus");
 
-	let firmwareFile = document.getElementById("idFile").files[0];
+		console.log("idBtnUpload_Click");
 
-	// Listen to the upload progress.
-	xhr.upload.onprogress = function(e) 
-	{
-		console.log("upload in onprogress", e);
-	};
-	
-	xhr.upload.loadstart = function(e)
-	{
-		console.log("upload started", e);	
-	};
+		var xhr = new XMLHttpRequest();
+		xhr.open('POST', '/ota/upload', true);
 
-	xhr.upload.loadend = function(e)
-	{
-		console.log("upload ended", e);	
-	};
+		let firmwareFile = document.getElementById("idFile").files[0];
 
-	xhr.upload.load  = function(e)
-	{
-		console.log("upload succeeded!", e);	
-	};
+		// Listen to the upload progress.
+		xhr.upload.onprogress = function(e)
+		{
+			lblStatus.innerHTML = "Uploading (" + ((e.loaded / e.total)*100).toFixed(2) + "%)";
+		};
 
-	xhr.upload.error = function(e)
-	{
-		console.log("upload error", e);	
-	};
+		xhr.onreadystatechange = function (e) {
+			if (xhr.readyState === 4) {
+				if (xhr.status === 200) {
+					console.log("Upload succeeded!", e);
+					lblStatus.innerHTML = "Upload completed!";
+				} else {
+					console.log("Error: ", e);
+					lblStatus.innerHTML = "Upload error: " + xhr.statusText;
+				}
+			}
+		};
 
-	xhr.upload.abort = function(e)
-	{
-		console.log("upload aborted", e);	
-	};
-	
-	xhr.send(firmwareFile);
+		lblStatus.innerHTML = "Uploading ...";
+		xhr.send(firmwareFile);
+	}
+	catch(err) {
+		lblStatus.innerHTML = "Upload error: " + err.message;
+	}
 }
 
 function AppLoaded()
--- Source/base-fw/main/www/js/app.js
+++ Source/base-fw/main/www/js/app.js
@@ -18,35 +18,35 @@ let allsymbols = [
   { id: 4, imgfile: "img/glyph/04.png", txt: '\u0044' },
   { id: 5, imgfile: "img/glyph/05.png", txt: '\u0045' },
   { id: 6, imgfile: "img/glyph/06.png", txt: '\u0046' },
-  
+
   { id: 7, imgfile: "img/glyph/07.png",  txt: '\u0047' },
   { id: 8, imgfile: "img/glyph/08.png",  txt: '\u0048' },
   { id: 9, imgfile: "img/glyph/09.png",  txt: '\u0049' },
   { id: 10, imgfile: "img/glyph/10.png", txt: '\u004A' },
   { id: 11, imgfile: "img/glyph/11.png", txt: '\u004B' },
   { id: 12, imgfile: "img/glyph/12.png", txt: '\u004C' },
-  
+
   { id: 13, imgfile: "img/glyph/13.png", txt: '\u004D' },
   { id: 14, imgfile: "img/glyph/14.png", txt: '\u004E' },
   { id: 15, imgfile: "img/glyph/15.png", txt: '\u004F' },
   { id: 16, imgfile: "img/glyph/16.png", txt: '\u0050' },
   { id: 17, imgfile: "img/glyph/17.png", txt: '\u0051' },
   { id: 18, imgfile: "img/glyph/18.png", txt: '\u0052' },
-  
+
   { id: 19, imgfile: "img/glyph/19.png", txt: '\u0053' },
   { id: 20, imgfile: "img/glyph/20.png", txt: '\u0054' },
   { id: 21, imgfile: "img/glyph/21.png", txt: '\u0055' },
   { id: 22, imgfile: "img/glyph/22.png", txt: '\u0056' },
   { id: 23, imgfile: "img/glyph/23.png", txt: '\u0057' },
   { id: 24, imgfile: "img/glyph/24.png", txt: '\u0058' },
-  
+
   { id: 25, imgfile: "img/glyph/25.png", txt: '\u0059' },
   { id: 26, imgfile: "img/glyph/26.png", txt: '\u005A' },
   { id: 27, imgfile: "img/glyph/27.png", txt: '\u0031' },
   { id: 28, imgfile: "img/glyph/28.png", txt: '\u0032' },
   { id: 29, imgfile: "img/glyph/29.png", txt: '\u0033' },
   { id: 30, imgfile: "img/glyph/30.png", txt: '\u0034' },
-  
+
   { id: 31, imgfile: "img/glyph/31.png", txt: '\u0035' },
   { id: 32, imgfile: "img/glyph/32.png", txt: '\u0036' },
   { id: 33, imgfile: "img/glyph/33.png", txt: '\u0037' },
@@ -73,7 +73,7 @@ let alladdresses = [
 
 var currentApp = new Vue({
   el: '#app',
-  data: 
+  data:
   {
     symbols: allsymbols,
     alladdresses: alladdresses,
@@ -84,4 +84,3 @@ function GetSymbolIndex(SymbolOneBased)
 {
   return allsymbols[SymbolOneBased-1];
 }
-
