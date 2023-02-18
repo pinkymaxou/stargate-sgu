b14755f6c339c92ad3851f525eddfd0126c1d9b2
Max <pinkymaxou08@gmail.com>
2023-02-17 19:46:30 -0500

    working prototype

--- Source/base-fw/main/SoundFX.c
+++ Source/base-fw/main/SoundFX.c
@@ -3,51 +3,78 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+static SOUNDFX_SFile m_sFiles[] =
+{
+    [SOUNDFX_EFILE_1_beginroll_mp3] = SOUNDFX_SFILE_INIT("1_beginroll.mp3", ""),
+    [SOUNDFX_EFILE_2_chevlck_wav]   = SOUNDFX_SFILE_INIT("2_chevlck.wav", ""),
+    [SOUNDFX_EFILE_3_chevlck2_wav]  = SOUNDFX_SFILE_INIT("3_chevlck2.wav", ""),
+    [SOUNDFX_EFILE_4_gateclos_mp3]  = SOUNDFX_SFILE_INIT("4_gateclos.mp3", ""),
+    [SOUNDFX_EFILE_5_gateopen_mp3]  = SOUNDFX_SFILE_INIT("5_gateopen.mp3", ""),
+    [SOUNDFX_EFILE_6_lggroll_mp3]   = SOUNDFX_SFILE_INIT("6_lggroll.mp3", ""),
+    [SOUNDFX_EFILE_7_lockfail_wav]  = SOUNDFX_SFILE_INIT("7_lockfail.wav", ""),
+    [SOUNDFX_EFILE_8_music1_mp3]    = SOUNDFX_SFILE_INIT("8_music1.mp3", ""),
+    [SOUNDFX_EFILE_9_music2_mp3]    = SOUNDFX_SFILE_INIT("9_music2.mp3", ""),
+};
+
+_Static_assert((sizeof(m_sFiles)/sizeof(m_sFiles[0])) == SOUNDFX_EFILE_Count, "coucou");
+
 void SOUNDFX_Init()
 {
-    GPIO_SendMp3PlayerCMD("AT+PLAYMODE=3\r\n");
+    SOUNDFX_Stop();
 }
 
 void SOUNDFX_ActivateGate()
 {
-    GPIO_SendMp3PlayerCMD("AT+PLAYNUM=1\r\n");
+    SOUNDFX_PlayFile(SOUNDFX_EFILE_1_beginroll_mp3);
 }
 
 void SOUNDFX_StartRollingSound()
 {
-    GPIO_SendMp3PlayerCMD("AT+PLAYNUM=6\r\n");
+    SOUNDFX_PlayFile(SOUNDFX_EFILE_6_lggroll_mp3);
 }
 
 void SOUNDFX_EngageChevron()
 {
-    GPIO_SendMp3PlayerCMD("AT+PLAYNUM=3\r\n");
+    SOUNDFX_PlayFile(SOUNDFX_EFILE_3_chevlck2_wav);
 }
 
 void SOUNDFX_ChevronLock()
 {
-    GPIO_SendMp3PlayerCMD("AT+PLAYNUM=2\r\n");
+    SOUNDFX_PlayFile(SOUNDFX_EFILE_2_chevlck_wav);
 }
 
 void SOUNDFX_Fail()
 {
-    GPIO_SendMp3PlayerCMD("AT+PLAYNUM=7\r\n");
+    SOUNDFX_PlayFile(SOUNDFX_EFILE_7_lockfail_wav);
 }
 
 void SOUNDFX_WormholeOpen()
 {
-    GPIO_SendMp3PlayerCMD("AT+PLAYNUM=5\r\n");
+    SOUNDFX_PlayFile(SOUNDFX_EFILE_5_gateopen_mp3);
 }
 
 void SOUNDFX_WormholeClose()
 {
-    GPIO_SendMp3PlayerCMD("AT+PLAYNUM=4\r\n");
+    SOUNDFX_PlayFile(SOUNDFX_EFILE_4_gateclos_mp3);
+}
+
+void SOUNDFX_PlayFile(SOUNDFX_EFILE eFile)
+{
+    const uint32_t u32Ix = (uint32_t)eFile + 1;
+    GPIO_SendMp3PlayerCMD("AT+PLAYMODE=3\r\n");
+    vTaskDelay(pdMS_TO_TICKS(10));  // Not sure how long it needs to take the command but it doesn't like being spammed
+    char szBuff[64];
+    sprintf(szBuff, "AT+PLAYNUM=%d\r\n", u32Ix);
+    GPIO_SendMp3PlayerCMD(szBuff);
 }
 
 void SOUNDFX_Stop()
 {
     // We don't know when it is playing and there is no explicit STOP command. It just toggle
     // so the trick is to play something then immediately put it on pause.
+    GPIO_SendMp3PlayerCMD("AT+PLAYMODE=3\r\n");
+    vTaskDelay(pdMS_TO_TICKS(10));
     GPIO_SendMp3PlayerCMD("AT+PLAYNUM=6\r\n");
-    vTaskDelay(pdMS_TO_TICKS(50));
+    vTaskDelay(pdMS_TO_TICKS(10));
     GPIO_SendMp3PlayerCMD("AT+PLAY=PP\r\n");
 }
--- Source/base-fw/main/SoundFX.h
+++ Source/base-fw/main/SoundFX.h
@@ -1,6 +1,29 @@
 #ifndef _SOUNDFX_H_
 #define _SOUNDFX_H_
 
+typedef enum
+{
+    SOUNDFX_EFILE_1_beginroll_mp3 = 0,
+    SOUNDFX_EFILE_2_chevlck_wav,
+    SOUNDFX_EFILE_3_chevlck2_wav,
+    SOUNDFX_EFILE_4_gateclos_mp3,
+    SOUNDFX_EFILE_5_gateopen_mp3,
+    SOUNDFX_EFILE_6_lggroll_mp3,
+    SOUNDFX_EFILE_7_lockfail_wav,
+    SOUNDFX_EFILE_8_music1_mp3,
+    SOUNDFX_EFILE_9_music2_mp3,
+
+    SOUNDFX_EFILE_Count
+} SOUNDFX_EFILE;
+
+typedef struct 
+{
+    const char* szName;
+    const char* szDesc;
+} SOUNDFX_SFile;
+
+#define SOUNDFX_SFILE_INIT(_szName, _szDesc) { .szName = _szName, .szDesc = _szDesc }
+
 void SOUNDFX_Init();
 
 void SOUNDFX_ActivateGate();
@@ -16,6 +39,8 @@ void SOUNDFX_WormholeOpen();
 void SOUNDFX_WormholeClose();
 
 void SOUNDFX_Fail();
+
+void SOUNDFX_PlayFile(SOUNDFX_EFILE eFile);
 
 void SOUNDFX_Stop();
 
