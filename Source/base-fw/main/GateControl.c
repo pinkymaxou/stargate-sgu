afbd20e211e33a1d501dbb29f3f41944b633c4a3
pinkymaxou <maxime.carrier.1987@gmail.com>
2023-09-30 23:17:49 -0400

    Feature/maxou/few improvements (#76)
    * cancel routine improved
    * update settings

--- Source/base-fw/main/GateControl.c
+++ Source/base-fw/main/GateControl.c
@@ -119,7 +119,7 @@ static void GateControlTask( void *pvParameters )
 
         if (m_eCurrentMode == GATECONTROL_EMODE_GoHome)
         {
-            SGUBRCOMM_ChevronLightning(&g_sSGUBRCOMMHandle, SGUBRPROTOCOL_ECHEVRONANIM_AllSymbolsOn);
+            SGUBRCOMM_ChevronLightning(&g_sSGUBRCOMMHandle, SGUBRPROTOCOL_ECHEVRONANIM_FadeIn);
             vTaskDelay(pdMS_TO_TICKS(750));
             if (DoHoming(&sProcCycle))
                 SGUBRCOMM_ChevronLightning(&g_sSGUBRCOMMHandle, SGUBRPROTOCOL_ECHEVRONANIM_FadeOut);
@@ -271,6 +271,7 @@ static bool AutoCalibrate(const SProcCycle* psProcCycle)
     // Move until it's outside
     GPIO_StartStepper();
     GPIO_ReleaseClamp();
+    SOUNDFX_Stop();
 
     const int32_t s32AttemptCount = 10;
     int32_t s32StepCount = 0;
@@ -366,6 +367,7 @@ static bool DoHoming(const SProcCycle* psProcCycle)
 
     m_bIsHomingCompleted = false;
 
+    SOUNDFX_Stop();
     GPIO_StartStepper();
     GPIO_ReleaseClamp();
     ESP_LOGI(TAG, "[DoHoming] Started");
@@ -523,6 +525,7 @@ void GATECONTROL_AnimRampLight(bool bIsActive)
 static bool DoDialSequence(const GATECONTROL_SDialArg* psDialArg, const SProcCycle* psProcCycle)
 {
     const char* szErrorString = "unknown";
+    bool bIsCancelled = false;
 
     if (!m_bIsHomingCompleted)
     {
@@ -544,16 +547,22 @@ static bool DoDialSequence(const GATECONTROL_SDialArg* psDialArg, const SProcCyc
     const int32_t s32StepPerRotation = NVSJSON_GetValueInt32(&g_sSettingHandle, SETTINGS_EENTRY_StepPerRotation);
     const uint32_t u32SymbBright = (uint32_t)NVSJSON_GetValueInt32(&g_sSettingHandle, SETTINGS_EENTRY_RingSymbolBrightness);
 
+    #define CHECK_CANCEL_JUMP \
+    do { \
+        if (m_bIsStop) \
+        { \
+            bIsCancelled = true; \
+            szErrorString = "cancelled"; \
+            goto ERROR; \
+        } \
+    } while(0)
+
     for(int i = 0; i < psDialArg->u8SymbolCount; i++)
     {
-        if (m_bIsStop)
-        {
-            szErrorString = "cancelled";
-            goto ERROR;
-        }
-
         const uint8_t u8SymbolIndex = psDialArg->u8Symbols[i];
         int32_t s32TicksTarget = GetAbsoluteSymbolTarget(u8SymbolIndex, s32StepPerRotation);
+
+        CHECK_CANCEL_JUMP;
 
         ESP_LOGI(TAG, "Go Home - Symbol: %d, previous target: %d, new target: %d, current: %d", /*0*/(int)u8SymbolIndex, /*1*/(int)m_s32Count, /*2*/(int)s32TicksTarget, /*3*/(int)m_s32Count);
 
@@ -569,6 +578,7 @@ static bool DoDialSequence(const GATECONTROL_SDialArg* psDialArg, const SProcCyc
         SOUNDFX_StartRollingSound();
         vTaskDelay(pdMS_TO_TICKS(250));
         MoveRelative(s32Move);
+        CHECK_CANCEL_JUMP;
         SOUNDFX_Stop();
         vTaskDelay(pdMS_TO_TICKS(150));
         SOUNDFX_EngageChevron();
@@ -603,7 +613,8 @@ static bool DoDialSequence(const GATECONTROL_SDialArg* psDialArg, const SProcCyc
     GPIO_StopStepper();
     SOUNDFX_Fail();
     GATECONTROL_AnimRampLight(false);
-    SGUBRCOMM_ChevronLightning(&g_sSGUBRCOMMHandle, SGUBRPROTOCOL_ECHEVRONANIM_ErrorToOff);
+    const SGUBRPROTOCOL_ECHEVRONANIM eChevronAnim = (bIsCancelled ? SGUBRPROTOCOL_ECHEVRONANIM_FadeOut : SGUBRPROTOCOL_ECHEVRONANIM_ErrorToOff);
+    SGUBRCOMM_ChevronLightning(&g_sSGUBRCOMMHandle, eChevronAnim);
     return false;
 }
 
