26c3a24e12ef19e04bf7d1da688bdb6303d67ff1
Max <pinkymaxou08@gmail.com>
2023-10-06 23:17:49 -0400

    fix helper macros

--- Source/base-fw/main/HelperMacro.h
+++
@@ -1,7 +0,0 @@
-#ifndef _HELPERMACRO_H_
-#define _HELPERMACRO_H_
-
-#define HELPERMACRO_MIN(a,b) (((a)<(b))?(a):(b))
-#define HELPERMACRO_MAX(a,b) (((a)>(b))?(a):(b))
-
-#endif
--- Source/common/components/SGU/HelperMacro.h
+++ Source/common/components/SGU/HelperMacro.h
@@ -5,4 +5,7 @@
 
 #define HELPERMACRO_LEDLOGADJ(_valuePerc,_maxPerc) ((pow(10.0f, _valuePerc) - 1.0f)/9.0f*_maxPerc)
 
+#define HELPERMACRO_MIN(a,b) (((a)<(b))?(a):(b))
+#define HELPERMACRO_MAX(a,b) (((a)>(b))?(a):(b))
+
 #endif
